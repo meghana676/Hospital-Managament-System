f2c0401bab96f9ddbddbff149d9994ec06065903
meghana676 <122991519+meghana676@users.noreply.github.com>
2024-03-28 18:45:02 +0530

    chNGES

--- src/Pages/Dashboard/Dashboard-Login/DLogin.jsx
+++ src/Pages/Dashboard/Dashboard-Login/DLogin.jsx
@@ -183,8 +183,8 @@ const DLogin = () => {
             <img src={admin} alt="profile" />
           </div>
           <div>
-            <p>ID - 100</p>
-            <p>Password - masai</p>
+            <p>ID - Techknowthon</p>
+            <p>Password - Kanyarasi</p>
             <form onSubmit={HandleSubmit}>
               <h3>{placement} ID</h3>
               <input
